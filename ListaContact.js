4c1671b97a71061624854540ce282e764f61a6b5
luzsierra07 <luzsierra07>
2023-04-13 21:35:32 -0500

    lista-actualizada

---
+++ ListaContact.js
@@ -0,0 +1,68 @@
+//Crear lista de cotactos, datos predefinidos
+let ListaContactos = [
+    {
+        id:1,
+        nombre: "Maria",
+        apellido: "Sierra",
+        telefono: "3143988342",
+        dirección: "cra 6 a",
+        ciudad: "Bogotá"
+    }, 
+    {
+        id:2,
+        nombre: "Lina",
+        apellido: "Meza",
+        telefono: "3209890059",
+        dirección: "cra 149",
+        ciudad: "Bogotá"
+    },
+    {
+        id:3,
+        nombre: "Elvia",
+        apellido: "Cordero",
+        telefono: "3128649289",
+        dirección: "calle 23",
+        ciudad: "Sincelejo" 
+    }
+];
+//crear una nueva función para agregar un nuevo contacto a la ista
+function agregarContacto (
+    id,
+    nombre,
+    apellido,
+    telefono,
+    dirección,
+    ciudad
+){ ListaContactos.push({
+        id: id,
+        nombre: nombre,
+        apellido: apellido,
+        telefono: telefono,
+        dirección: dirección,
+        ciudad: ciudad
+})        
+}
+//función para borrar un contacto de la lista
+function borrarContacto (id){
+    let indice = ListaContactos.findIndex((contacto)=>contacto.id===id);
+    if (indice !== -1){
+        ListaContactos.splice(indice, 1);
+    }
+}
+//Crea una función para imprimir en consola los contactos presentes en la lista
+function imprimirContactos(){
+    console.log(ListaContactos);
+}
+//agregar contacto nuevo
+agregarContacto(
+    4,
+    "Elvis",
+    "Diaz",
+    "3013740259",
+    "calle 22",
+    "Planeta Rica"
+);
+//borrar contacto
+borrarContacto(2);
+//imprimir lista de contactos actualizada
+imprimirContactos();
